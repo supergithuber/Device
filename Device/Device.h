e712b7275c18ccd88e267b4b670a85f4d46e508e
sivanWu <1984430988@qq.com>
2018-06-02 12:37:05 +0800

    support carthage

---
+++ Device/Device.h
@@ -0,0 +1,19 @@
+//
+//  Device.h
+//  Device
+//
+//  Created by 吴浠 on 2018/6/2.
+//  Copyright © 2018年 wuxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Device.
+FOUNDATION_EXPORT double DeviceVersionNumber;
+
+//! Project version string for Device.
+FOUNDATION_EXPORT const unsigned char DeviceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Device/PublicHeader.h>
+
+
